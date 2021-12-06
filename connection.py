26dfd731eba3ad0fb194bd9d5112442611b67653
R3qUi3M86 <requiem123@gmail.com>
2021-12-06 20:10:00 +0100

    implemented list questions as per specification

---
+++ connection.py
@@ -0,0 +1,46 @@
+import csv
+import os
+
+QUESTION_DATA_FILE_PATH = 'sample_data/question.csv'
+ANSWER_DATA_FILE_PATH = 'sample_data/answer.csv'
+QUESTION_DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+ANSWER_DATA_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+
+
+def get_all_data_from_file(file_name, separator='#'):
+    if file_name == QUESTION_DATA_FILE_PATH:
+        headers = QUESTION_DATA_HEADER
+    else:
+        headers = ANSWER_DATA_HEADER
+
+    try:
+        with open(file_name, "r") as file:
+            lines = file.readlines()
+
+        data = []
+        for element in lines:
+            elements_list = element.replace("\n", "").split(separator)
+            element_dict = {}
+            for i, label in enumerate(headers):
+                element_dict[label] = elements_list[i]
+
+            data.append(element_dict)
+        return data
+
+    except IOError:
+        return []
+
+
+def write_table_to_file(table, file_name, separator='#'):
+    if file_name == QUESTION_DATA_FILE_PATH:
+        headers = QUESTION_DATA_HEADER
+    else:
+        headers = ANSWER_DATA_HEADER
+
+    with open(file_name, "w") as file:
+        for record_dict in table:
+            values_row = []
+            for label in headers:
+                values_row.append(record_dict[label])
+            row = separator.join(values_row)
+            file.write(row + "\n")
--- server.py
+++ server.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import data_manager
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/list")
+def list_questions():
+    users_questions = data_manager.get_all_questions()
+    questions_sorted_by_timestamp = data_manager.sort_data_by_timestamp(users_questions)
+    questions_timestamp_converted = data_manager.convert_timestamp_to_date_in_data(questions_sorted_by_timestamp)
+    headers_list = data_manager.get_questions_headers()
+
+    return render_template('list.html', questions=questions_timestamp_converted, headers=headers_list)
+
+
+@app.route("/add-question")
+def route_ask_question():
+    pass
 
 
 if __name__ == "__main__":
