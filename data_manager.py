3d5648af9f96ccd4ddbea2b948199f719bcd6bb5
R3qUi3M86 <requiem123@gmail.com>
2021-12-07 21:09:05 +0100

    implemented questions sorting options

--- data_manager.py
+++ data_manager.py
@@ -1,15 +1,20 @@
 import connection
 from datetime import datetime
 
+ASCENDING = "ascending"
+DESCENDING = "descending"
+
 
 def get_all_questions():
     return connection.get_all_data_from_file(connection.QUESTION_DATA_FILE_PATH)
 
 
-def sort_data_by_timestamp(data):
+def sort_data(data, direction, ordering_key):
     for _ in range(len(data)):
         for i, _ in enumerate(range(len(data)-1)):
-            if data[i]['submission_time'] > data[i+1]['submission_time']:
+            if direction == DESCENDING and data[i][ordering_key] < data[i+1][ordering_key]:
+                data[i], data[i+1] = data[i+1], data[i]
+            elif direction == ASCENDING and data[i][ordering_key] > data[i+1][ordering_key]:
                 data[i], data[i+1] = data[i+1], data[i]
     return data
 
@@ -20,6 +25,7 @@ def convert_timestamp_to_date_in_data(data):
         date = datetime.fromtimestamp(timestamp)
         row['submission_time'] = date
     return data
+
 
 def get_questions_headers():
     return connection.QUESTION_DATA_HEADER
--- server.py
+++ server.py
@@ -1,22 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
 import data_manager
+from data_manager import ASCENDING, DESCENDING
 
 app = Flask(__name__)
+display_dict = {}
+
+ORDER_DIRECTION = 'order_direction'
+ORDER_DIRECTIONS = {ASCENDING: 'Ascending', DESCENDING: 'Descending'}
+
+ORDER_BY = 'order_by'
+SORTING_MODES = {'submission_time': 'Time added',
+                 'view_number': 'Views',
+                 'vote_number': 'Votes',
+                 'title': 'Title',
+                 'message': 'Message'}
 
 
 @app.route("/")
 @app.route("/list")
 def list_questions():
     users_questions = data_manager.get_all_questions()
-    questions_sorted_by_timestamp = data_manager.sort_data_by_timestamp(users_questions)
-    questions_timestamp_converted = data_manager.convert_timestamp_to_date_in_data(questions_sorted_by_timestamp)
     headers_list = data_manager.get_questions_headers()
 
-    return render_template('list.html', questions=questions_timestamp_converted, headers=headers_list)
+    order_by = request.args.get(ORDER_BY, 'submission_time')
+    order_direction = request.args.get(ORDER_DIRECTION, DESCENDING)
+
+    questions_sorted = data_manager.sort_data(users_questions, order_direction, order_by)
+    print(questions_sorted)
+    questions_converted = data_manager.convert_timestamp_to_date_in_data(questions_sorted)
+
+    return render_template('list.html',
+                           questions=questions_converted,
+                           headers=headers_list,
+                           sorting_modes=SORTING_MODES,
+                           sorting_direction=ORDER_DIRECTIONS,
+                           current_ordering=order_by,
+                           current_direction=order_direction)
+
 
 @app.route("/question/<question_id>")
 def route_display_question(question_id):
+    display_dict[question_id] += 1
     pass
+
 
 @app.route("/add-question")
 def route_ask_question():
